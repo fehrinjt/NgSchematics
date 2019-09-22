09e786aead81c3ac10c26d2d9b8292c81ac50365
Jeff <jeff.fehringer@gmail.com>
2019-09-22 15:44:29 -0600

    reading json for html output

---
+++ src/test/form-property.interface.ts
@@ -0,0 +1,8 @@
+export interface FormProperty {
+    name: string;
+    label: string;
+    type: string;
+    required: boolean;
+    minLength: number;
+    maxLength: number;
+}
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,10 +1,29 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, url, apply, template, mergeWith } from '@angular-devkit/schematics';
+import { FormProperty } from './form-property.interface';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function test(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+
+    const { path } = _options;
+    const contents = tree.read(path);
+
+    if (contents) {
+      const sourceTemplates = url('./files');
+
+      const props = <FormProperty[]>JSON.parse(contents.toString());
+
+      const sourceParamTemplates = apply(sourceTemplates, [
+        template({
+          props
+        })
+      ]);
+
+      return mergeWith(sourceParamTemplates);
+    }
+    
     return tree;
   };
 }
